9c06f6b4e37e61e98bd880a987dd4141d7f779b6
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-09-22 16:33:29 +0500

    Add files via upload

---
+++ src/scheduler.py
@@ -0,0 +1,75 @@
+from utils import SimulationParams
+from numpy.random import uniform
+
+class GenericScheduler():
+    def __init__(self, simulationParams = None):
+        pass
+    def getNextPath(self):
+        raise Exception("Not implemented")
+
+class WeightedRoundRobinScheduler(GenericScheduler):
+    def __init__(self, simulationParameters = None):
+        self.params = simulationParameters
+        self.counter = 0
+        self.totalBandwidth = 0
+        self.batchSize = 0
+        self.paths = self.params.getPaths()
+        for path in self.paths:
+            self.totalBandwidth += path.bandwidth()
+
+        self.cumulativeBatch = []
+        i = 0
+        for path in self.paths:
+            if i == 0:
+                self.cumulativeBatch.append(int(self.totalBandwidth / path.bandwidth()))
+            else:
+                self.cumulativeBatch.append(self.cumulativeBatch[i - 1] + \
+                    int(self.totalBandwidth / path.bandwidth()))
+            self.batchSize += int(self.totalBandwidth / path.bandwidth())
+            i += 1
+
+    def getNextPath(self):
+        self.counter = self.counter % self.batchSize;
+        nextPath = None
+        for index in range(0, len(self.cumulativeBatch)):
+            if self.counter < self.cumulativeBatch[index]:
+                nextPath = self.paths[index]
+                break;
+        self.counter += 1
+        return nextPath
+
+class BernoulliScheduler(GenericScheduler):
+    def __init__(self, simulationParameters = None):
+        self.params = simulationParameters
+        self.totalBandwidth = 0
+        self.paths = self.params.getPaths()
+        for path in self.paths:
+            self.totalBandwidth += path.bandwidth()
+
+        self.cumulativeProbs = []
+        i = 0
+        for path in self.paths:
+            if i == 0:
+                self.cumulativeProbs.append(path.bandwidth() / self.totalBandwidth)
+            else:
+                self.cumulativeProbs.append(self.cumulativeProbs[i - 1] + \
+                    path.bandwidth() / self.totalBandwidth)
+            i += 1
+
+    def getNextPath(self):
+        nextPath = None
+        prob = uniform(0, 1)
+        for index in range(0, len(self.paths)):
+            if prob <= self.cumulativeProbs[index]:
+                nextPath = self.paths[index]
+                break;
+        return nextPath
+
+class SchedulerFactory():
+    @staticmethod
+    def getInstance(params):
+        if params.getSchedulerType() == "wrr":
+            return WeightedRoundRobinScheduler(params)
+        if params.getSchedulerType() == "bn":
+            return BernoulliScheduler(params)
+        raise Exception("Unsupported scheduler type")
---
+++ src/utils.py
@@ -0,0 +1,115 @@
+from delays import GenericDelay
+from delays import DelayFactory
+
+class Analyzer():
+    def __init__(self, packets):
+        self.packets = packets
+        self.sequence = self.__sort__()
+    def __sort__(self):
+        output = []
+        ts = []
+        for idx1 in range(0, len(self.packets)):
+            for idx2 in range(0, len(self.packets)):
+                if self.packets[idx1].arrivalTimestamp < self.packets[idx2].arrivalTimestamp:
+                    tmp = self.packets[idx1]
+                    self.packets[idx1] = self.packets[idx2]
+                    self.packets[idx2] = tmp
+        for idx in range(0, len(self.packets)):
+            output.append(self.packets[idx].sequence)
+            ts.append(self.packets[idx].arrivalTimestamp)
+        return output
+    
+    def computeReorderingDistance(self):
+        cumulative = 0
+        for i in range(0, len(self.sequence) - 2):
+            maxrm = 0
+            for j in range(i + 1, len(self.sequence)):
+                if self.sequence[i] > self.sequence[j]:
+                    rm = j - i
+                    if rm > maxrm:
+                        maxrm = rm
+            cumulative += maxrm
+        return cumulative / len(self.sequence)
+
+    def getSequence(self):
+        return self.sequence
+
+class Path():
+    def __init__(self, index, distribution, args):
+        self.index = index;
+        self.distribution = distribution;
+        self.delay = DelayFactory.get(distribution, args)
+        self.lastTimestamp = 0
+    def pathIndex(self):
+        return self.index;
+    def bandwidth(self):
+        return 1.0/self.delay.mean()
+    def delayMean(self):
+        return self.delay.mean()
+    def getDelay(self):
+        return self.delay.generate()
+    def getLastTimestamp(self):
+        return self.lastTimestamp
+    def setLastTimestamp(self, ts):
+        self.lastTimestamp = ts
+
+class Packet():
+    def __init__(self, sequence, arrivalTimestamp, pathIndex):
+        self.sequence = sequence
+        self.arrivalTimestamp = arrivalTimestamp
+        self.pathIndex = pathIndex
+    
+class SimulationParams():
+    def __init__(self, numRounds = 1000, numPackets = 1000, numPaths = 2, scheduler = "wrr", distribution = "exponential", distributionParams = [0.3, 0.3]):
+        self.numRounds = numRounds
+        self.numPackets = numPackets
+        self.numPaths = numPaths
+        self.distribution = distribution
+        self.distributionParams = distributionParams
+        self.scheduler = scheduler
+        self.paths = [];
+    def getNumPackets(self):
+        return self.numPackets
+    def getNumRounds(self):
+        return self.numRounds
+    def getNumPaths(self):
+        return self.numPaths
+    def getDistribution(self):
+        return self.distributionParams
+    def getSchedulerType(self):
+        return self.scheduler
+    def getPaths(self):
+        if len(self.paths) > 0:
+            return self.paths
+        for i in range(0, self.numPaths):
+            self.paths.append(Path(i, self.distribution, self.distributionParams))
+        return self.paths
+
+class ParamsParser():
+    def __init__(self, file):
+        self.maxSimulations = 0
+        self.simulations = [];
+
+        with open(file) as fd:
+            lines = fd.readlines();
+            c = 0
+            for line in lines:
+                if c % 2 == 0:
+                    p = line.split(" ")
+                    numPaths = int(p[0])
+                    numRounds = int(p[1])
+                    numPackets = int(p[2])
+                else:
+                    p = line.split(" ")
+                    distribution = p[0]
+                    schedulerType = p[1]
+                    args = []
+                    for i in range(2, len(p)):
+                        args.append(float(p[i]))
+                if c % 2 == 1:
+                    self.simulations.append(SimulationParams(numRounds, numPackets, numPaths, schedulerType, distribution, args))
+                c += 1
+    def getNumberOfSimulations(self):
+        return len(self.simulations)
+    def getSimulationParams(self, index):
+        return self.simulations[index]
