45ebd414c3d0ce05d2017ab756a556772ea52427
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-09-22 16:38:34 +0500

    Delete run_analysis.sh

--- run_analysis.sh
+++
@@ -1,2 +0,0 @@
-/cygdrive/c/Program\ Files/R/R-4.1.2/bin/R.exe --no-restore --no-save --slave --args bn_identical.csv wrr_identical.csv reordering_identical.pdf < plot_reordering.R
-/cygdrive/c/Program\ Files/R/R-4.1.2/bin/R.exe --no-restore --no-save --slave --args bn_similar.csv wrr_similar.csv reordering_similar.pdf < plot_reordering.R
