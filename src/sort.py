b75c7fdea134df53b9fea6815018f6e1155d8c9b
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-09-23 14:34:51 +0500

    Fixed sorting

--- src/sort.py
+++ src/sort.py
@@ -1,8 +1,8 @@
 a = [2, 1, 3, 0]
 def sort(a):
 	for i in range(0, len(a)):
-		for j in range(0, len(a)):
-			if a[i] < a[j]:
+		for j in range(i, len(a)):
+			if a[i] > a[j]:
 				tmp = a[i]
 				a[i] = a[j]
 				a[j] = tmp
