7c20ab3a11bc6a93451b257e6ad119e34140b356
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-09-23 14:34:40 +0500

    Added more simulation configurations

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,2 +1,3 @@
 /cygdrive/c/Program\ Files/R/R-4.1.2/bin/R.exe --no-restore --no-save --slave --args bn_identical.csv wrr_identical.csv reordering_identical.pdf < plot_reordering.R
 /cygdrive/c/Program\ Files/R/R-4.1.2/bin/R.exe --no-restore --no-save --slave --args bn_similar.csv wrr_similar.csv reordering_similar.pdf < plot_reordering.R
+/cygdrive/c/Program\ Files/R/R-4.1.2/bin/R.exe --no-restore --no-save --slave --args 2paths_bn.txt 2paths_wrr.txt 2paths_similar.pdf < ../scripts/plot_reordering.R
